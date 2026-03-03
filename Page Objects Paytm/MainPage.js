9f4f69047496e925ab2f92c29c0b782c869f6663
Vardhaman Patil/LGSI VS Quality <vardhaman.patil@lge.com>
2026-03-03 20:06:09 +0530

    updated POM

--- Page Objects Paytm/MainPage.js
+++ Page Objects Paytm/MainPage.js
@@ -2,7 +2,17 @@ class MainPage{
 
     constructor(){
         this.page=this.page;
+        
+        //Recharge & Bills
         this.recharge= this.page.locator("//li[@class='_2Szju'  and contains(., 'Recharge & Bills')]");
+        this.mobileRecharge= this.page.locator("//li[text()='Recharge & Bills']//preceding::a[text()='Mobile Recharge']");
+
+        //Payments & Services
+        this.paymsAndServ= page.locator("//li[@class='_2Szju' and contains(., 'Payments & Services')]");
+        this.paymnt= this.page.locator("//li[text()='Payments & Services']//preceding::a[text()='Payments']");
+        //bills paymnt and recharges
+        this.Bpr= this.page.locator("//a[@href='https://paytm.com/recharge-bill-payment']");
+
 
     }
 
--- tests/paytme2e.spec.js
+++ tests/paytme2e.spec.js
@@ -3,5 +3,7 @@ import {test, expect} from '@playwright/test';
 test("TC to navigate through Paytm site", async({page})=>{
 
     await page.goto("https://paytm.com/", {waitUntil:'domcontentloaded'});
+    await page.pause();
+    await page.getByText("Payments & Services").click();
     
 })
